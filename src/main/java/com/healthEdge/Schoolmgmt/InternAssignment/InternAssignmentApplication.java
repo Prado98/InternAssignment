8a7153b15efe2f78bac6e673ae15349b8714da35
Prado98 <pradeepsudars.98@gmail.com>
2019-03-01 17:29:46 +0530

    Initializing Project

---
+++ src/main/java/com/healthEdge/Schoolmgmt/InternAssignment/InternAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.healthEdge.Schoolmgmt.InternAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthEdge/Schoolmgmt/InternAssignment/InternAssignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.healthEdge.Schoolmgmt.InternAssignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InternAssignmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
