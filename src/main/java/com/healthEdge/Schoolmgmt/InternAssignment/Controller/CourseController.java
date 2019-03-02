9f1d08f4a8deaf1d9e4a1299cc6f39c6684a686e
rashu259710 <43058175+rashu259710@users.noreply.github.com>
2019-03-02 23:48:45 +0530

    yet to finiah

--- src/main/java/com/healthEdge/Schoolmgmt/InternAssignment/Controller/CourseController.java
+++ src/main/java/com/healthEdge/Schoolmgmt/InternAssignment/Controller/CourseController.java
@@ -1,14 +1,14 @@
 package com.healthEdge.Schoolmgmt.InternAssignment.Controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping(/Student);
-public class StudentController {
+@RequestMapping("/Course")
+public class CourseController {
     @Autowired
-
+    private Course
 
 }
--- src/main/java/com/healthEdge/Schoolmgmt/InternAssignment/Controller/TeacherController.java
+++ src/main/java/com/healthEdge/Schoolmgmt/InternAssignment/Controller/TeacherController.java
@@ -1,14 +1,12 @@
 package com.healthEdge.Schoolmgmt.InternAssignment.Controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping(/Student);
-public class StudentController {
-    @Autowired
-
-
+@RequestMapping("/Teacher")
+public class TeacherController {
+@Autowired
 }
