acddfe7900ddfc8ffb09bb5bd0f9aea867f4039a
kasiviswanath <kasivisu4@gmail.com>
2019-03-04 09:14:45 +0530

    Update SchoolRepository.java

--- src/main/java/com/healthEdge/Schoolmgmt/InternAssignment/Repository/SchoolRepository.java
+++ src/main/java/com/healthEdge/Schoolmgmt/InternAssignment/Repository/SchoolRepository.java
@@ -2,7 +2,7 @@ package com.healthEdge.Schoolmgmt.InternAssignment.Repository;
 
 import com.healthEdge.Schoolmgmt.InternAssignment.Entity.School;
 import com.healthEdge.Schoolmgmt.InternAssignment.Entity.Student;
-import com.sun.org.apache.xpath.internal.operations.String;
+//import com.sun.org.apache.xpath.internal.operations.String;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SchoolRepository extends JpaRepository<School, Integer> {
