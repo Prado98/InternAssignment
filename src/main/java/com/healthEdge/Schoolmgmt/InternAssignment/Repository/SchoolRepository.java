d55109dc94a68c14d732a21d61a51d3130994eb9
Madhuvani321 <48146974+Madhuvani321@users.noreply.github.com>
2019-03-04 09:16:56 +0530

    Update SchoolRepository.java

--- src/main/java/com/healthEdge/Schoolmgmt/InternAssignment/Repository/SchoolRepository.java
+++ src/main/java/com/healthEdge/Schoolmgmt/InternAssignment/Repository/SchoolRepository.java
@@ -7,19 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SchoolRepository extends JpaRepository<School, Integer> {
     School findAllById(int schoolId);
-
     School findAllByName(String schoolName);
     //School findAllBySchoolAddress(String schoolName);
-
     School findAllByEmail(String schoolEmail);
-
     School findAllByAddress(String schoolAddress);
-
     void deleteById(String schoolEmail);
-
     void deleteByName(String schoolName);
-
     void deleteByAddress(String schoolAddress);
-
     void deleteByEmail(String schoolEmail);
 }
